fa41ea172f90001ab3781faeabd7b65a479ba8b4
Thamilarasan <thamilarasan.gp2023it@sece.ac.in>
2025-09-20 12:33:02 +0530

    base url changed

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 MONGO_URI = "mongodb+srv://thamilprakasam2005:appichithamil@cluster0.qqwny.mongodb.net/url_shortener"
-BASE_URL = "https://linkly-zo8a.onrender.com"
+BASE_URL = "https://linkly-zo8a.onrender.com/"
